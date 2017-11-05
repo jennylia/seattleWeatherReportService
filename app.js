0874f53be0cc2e3c73a857b24eabca3dd7841ab2
Jenny Lian <jennylian100@gmail.com>
2017-11-04 21:37:15 -0700

    seattle weather service

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send({
-    "Output": "Hello World!"
-  });
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+const apiKey = '3d86fd1c772eefd96c501522e55d2b98';
+
+
+app.get('/', function (req, res) {
+    let city = "seattle";
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+    request(url, function (err, response, body) {
+        if (err) {
+            res.send({
+                "Output": "Error"
+            });
+        } else {
+            let weather = JSON.parse(body)
+            if (weather.main == undefined) {
+                res.send({
+                    "Output": "Error"
+                });
+            } else {
+                console.log(weather);
+                let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+                res.send({
+                    "Output": weatherText
+                });
+            }
+        }
+    });
 });
 
-app.post('/', function(req, res) {
-  res.send({
-    "Output": "Hello World!"
-  });
+app.post('/', function (req, res) {
+    res.send({
+        "Output": "Hello World!"
+    });
 });
 
 
